731af8522e1f312a34283d6f872e3470435a002c
Nelson Feng <122589170+n-elsonf@users.noreply.github.com>
2025-04-03 16:08:49 -0400

    Add solution for unknown: Two Sum - LeetCode

--- unknown_two-sum---leetcode.py
+++ unknown_two-sum---leetcode.py
@@ -1,1 +1,1 @@
-        for i in range(len(nums)):            num = nums[i]            compliment = target - num            if compliment in dct:                return [i, dct[compliment]]            dct[num] = i
+        dct = {} # dictionary where key represents the number from the current iterated i in nums and value is the index needed to add to the target    def twoSum(self, nums: List[int], target: int) -> List[int]:class Solution:
